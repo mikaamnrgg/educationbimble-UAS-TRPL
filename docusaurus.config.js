1244dae7f155fed1fa730115d96d9d856b418ba9
mikaamnrgg <118428432+mikaamnrgg@users.noreply.github.com>
2023-01-24 19:34:52 +0700

    ubah

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
       navbar: {
         title: 'Education Bimble',
         logo: {
-          src: 'image.png',
+          src: 'https://id.images.search.yahoo.com/search/images;_ylt=Awr1TkOgzs9jfrUsMzPLQwx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=gambar+education+svg&fr2=piv-web&type=E210ID885G91712&fr=mcafee#id=1&iurl=http%3A%2F%2Fnationals.org.au%2Fwp-content%2Fuploads%2F2016%2F05%2FEducation.png&action=click',
         },
         items: [
           {
