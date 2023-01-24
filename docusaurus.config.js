0d39f42287b2ee8302d6884180cabc8fd53113ab
mikaamnrgg <118428432+mikaamnrgg@users.noreply.github.com>
2023-01-24 19:26:38 +0700

    ganti

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Education',
+  title: 'Education Bimble',
   tagline: 'From groceries to goals, we have got you covered.',
   url: 'https://educationbimble-mika.vercel.app//',
   baseUrl: '/',
