67a71121734cda795451bc09c59609ea7364a29c
mikaamnrgg <118428432+mikaamnrgg@users.noreply.github.com>
2023-01-24 18:47:58 +0700

    ganti

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'To-do list application',
   tagline: 'From groceries to goals, we have got you covered.',
-  url: 'https://educationbimble-UAS-TRPL.vercel.app/',
+  url: 'https://educationbimble-uas-trpl.vercel.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
